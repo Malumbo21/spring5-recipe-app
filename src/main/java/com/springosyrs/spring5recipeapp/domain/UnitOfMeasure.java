6efa5a2a515161ccbf9875f42c7c667572646dfe
Malumbo Sinkamba <Malumbo@chrilantech.com>
2022-08-08 03:07:14 +0200

    First commit

---
+++ src/main/java/com/springosyrs/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package com.springosyrs.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String uom;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
